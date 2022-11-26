676504a23db77d723fff3a00af28030049d0d5af
nasser <abdelnasserahmed96@gmail.com>
2022-11-26 07:44:17 +0200

    Create HomeResident serializer and finishing create home resident

--- home_residents/serializers.py
+++ home_residents/serializers.py
@@ -1,12 +1,43 @@
+import re
 from rest_framework import serializers
-#from .models import HomeResident
+from .models import HomeResident, User
 
 
-"""class HomeResidentSerializer(serializers.Serializer):
+class HomeResidentSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20)
+    first_name = serializers.CharField(max_length=15)
+    last_name = serializers.CharField(max_length=15)
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=30)
+    phone_number = serializers.CharField(max_length=15)
+
+    def validate_phone_number(self, phone_number):
+        if not re.match("^(\+[0-9]{1,3}){0,1}([0-9]{11,13})$", phone_number):
+            raise serializers.ValidationError("Incorrect phone number format, plase use +country_code followed by the phone number")
+        return phone_number
+
     def create(self, validated_data):
         print('Validated data: ')
         print(validated_data)
-        return HomeResident.objects.create(**validated_data)
+        home_resident = {
+            'user': None,
+            'phone_number': validated_data.pop('phone_number')
+        }
+        new_user = User.objects.create_user(**validated_data)
+        home_resident['user'] = new_user
+        new_home_resident = HomeResident.objects.create(**home_resident)
+        return new_home_resident
+
+    def to_representation(self, instance):
+        response = dict()
+        response['username'] = instance.user.username
+        response['first_name'] = instance.user.first_name
+        response['last_name'] = instance.user.last_name
+        response['email'] = instance.user.email
+        response['phone_number'] = instance.phone_number
+        return response
+
     class Meta:
         model = HomeResident
-        fields = '__all__'"""
+        exclude = ['user']
+
--- home_residents/views.py
+++ home_residents/views.py
@@ -6,8 +6,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework_simplejwt.tokens import AccessToken
-#from .models import HomeResident
-#from .serializers import HomeResidentSerializer
+from .models import HomeResident
+from .serializers import HomeResidentSerializer
 
 
 # Create your views here.
@@ -29,13 +29,14 @@ def get_user_details(request):
 @ensure_csrf_cookie
 def create_home_resident(request):
     print('Request data: ', request.data)
-"""    serializer = HomeResidentSerializer(data=request.data, many=False)
+    serializer = HomeResidentSerializer(data=request.data, many=False)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)"""
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-"""@api_view(['GET'])
+
+@api_view(['GET'])
 def get_all_home_residents(request):
     home_residents_serializer = HomeResidentSerializer(HomeResident.objects.all(), many=True)
-    return Response(home_residents_serializer.data, status=status.HTTP_200_OK)"""
+    return Response(home_residents_serializer.data, status=status.HTTP_200_OK)
